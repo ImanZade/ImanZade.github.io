b2fc3c712abf4aefcb65e40d680e4653caf0d113
ImanZade <imanzade313@hotmail.com>
2018-10-07 18:57:33 +0400

    new responsive menu

---
+++ javascript hamburger menu/main.js
@@ -0,0 +1,22 @@
+$(window).scroll(function(){
+    if($(document).scrollTop() > 50){
+      $('nav').addClass('scroll');
+    } else {
+      $('nav').removeClass('scroll');
+    }
+});
+
+
+$(window).scroll(function(){
+    if($(document).scrollTop() > 100){
+      $('#ads').addClass('ads');
+    } else {
+      $('#ads').removeClass('ads');
+    }
+});
+
+$(document).ready(function(){
+  $('.menu').click(function() {
+    $('ul').toggleClass('active');
+  })
+})
